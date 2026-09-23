1218ba079d64de58048945f56289ae818e1a9511
Owais Raza Shaheen <greenman9909@gmail.com>
2026-09-24 01:01:52 +0200

    Add runtime task queue

---
+++ src/runtime/task_queue.py
@@ -0,0 +1,11 @@
+"""Task queue foundation for workflow execution."""
+
+class TaskQueue:
+    def __init__(self):
+        self.tasks = []
+
+    def push(self, task):
+        self.tasks.append(task)
+
+    def pop(self):
+        return self.tasks.pop(0) if self.tasks else None
