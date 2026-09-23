b3848d4ab023331cf31bb6d6fe22f75e15cc9312
Owais Raza Shaheen <greenman9909@gmail.com>
2026-09-24 00:33:14 +0200

    Add internal event bus

---
+++ src/core/event_bus.py
@@ -0,0 +1,10 @@
+class EventBus:
+    def __init__(self):
+        self.listeners = {}
+
+    def on(self, event, callback):
+        self.listeners.setdefault(event, []).append(callback)
+
+    def emit(self, event, data=None):
+        for callback in self.listeners.get(event, []):
+            callback(data)
